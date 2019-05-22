ded44a1cdb4d207007443c1047bf8b054529f29e
skytalemcc <79785200@163.com>
2019-05-22 14:30:06 +0000

    add the goroutines and channles

---
+++ class_tour/test4concurrency/channels.go
@@ -0,0 +1,163 @@
+package main
+
+/***
+信道可以想像成 Go 协程之间通信的管道。如同管道中的水会从一端流到另一端，通过使用信道，数据也可以从一端发送，在另一端接收。
+所有信道都关联了一个类型。信道只能运输这种类型的数据，而运输其他类型的数据都是非法的。
+chan T 表示 T 类型的信道。
+信道的零值为 nil。信道的零值没有什么用，应该像对 map 和切片所做的那样，用 make 来定义信道。
+
+信道是带有类型的管道，你可以通过它用信道操作符 <- 来发送或者接收值。
+ch <- v    // 将 v 发送至信道 ch。
+v := <-ch  // 从 ch 接收值并赋予 v。
+箭头就是数据流的方向。
+通过信道发送和接收数据。
+data := <- chan // 读取信道 chan
+chan <- data // 写入信道 chan
+
+默认情况下，发送和接收操作在另一端准备好之前都会阻塞。
+这使得 Go 程可以在没有显式的锁或竞态变量的情况下进行同步。
+
+发送与接收默认是阻塞的。这是什么意思？
+当把数据发送到信道时，程序控制会在发送数据的语句处发生阻塞，直到有其它 Go 协程从信道读取到数据，才会解除阻塞。
+与此类似，当读取信道的数据时，如果没有其它的协程把数据写入到这个信道，那么读取过程就会一直阻塞着。
+信道的这种特性能够帮助 Go 协程之间进行高效的通信，不需要用到其他编程语言常见的显式锁或条件变量。
+
+简单理解 就是，发送的时候或者读取的时候，如果没有消费掉，信道就会造成阻塞，协程通过信道不管是读还是写。结束后，信道才停止阻塞，进入主进程。
+***/
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // 将和送入 c
+}
+
+//执行完毕后，关闭信道。
+func producer(chnl chan int) {
+	for i := 0; i < 10; i++ {
+		chnl <- i
+	}
+	close(chnl)
+}
+
+func main() { //声明
+	var a chan int
+	if a == nil {
+		fmt.Println("channel a is nil, going to define it")
+		a = make(chan int) //使用前创建
+		//简短声明通常也是一种定义信道的简洁有效的方法。
+		//a := make(chan int)
+		fmt.Printf("Type of a is %T\n", a)
+	}
+
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c) //协程计算写入信道
+	go sum(s[len(s)/2:], c) //协程计算写入信道，会占用信道，阻塞信道，知道协程结束
+	x, y := <-c, <-c        // 从 c 中读取数据。 主协程，这个步骤 要等到两个协程执行完毕后，信道才会结束阻塞。不阻塞才能执行下一个语句。
+	fmt.Println(x, y, x+y)
+
+	//死锁
+	//使用信道需要考虑的一个重点是死锁。
+	//当 Go 协程给一个信道发送数据时，照理说会有其他 Go 协程来接收数据。如果没有的话，程序就会在运行时触发 panic，形成死锁。
+	//同理，当有 Go 协程等着从一个信道接收数据时，我们期望其他的 Go 协程会向该信道写入数据，要不然程序就会触发 panic。
+	//由于它超出了信道的容量，因此这次写入发生了阻塞。现在想要这次写操作能够进行下去，必须要有其它协程来读取这个信道的数据。
+	/***
+	信道可以是 带缓冲的。将缓冲长度作为第二个参数提供给 make 来初始化一个带缓冲的信道：
+	ch := make(chan int, 100) 仅当信道的缓冲区填满后，向其发送数据时才会阻塞。当缓冲区为空时，接受方会阻塞。
+
+	ch1 := make(chan int) 会发生死锁，因为信道空间占满，阻塞。
+
+	无缓冲信道的发送和接收过程是阻塞的。
+	创建一个有缓冲（Buffer）的信道。
+	只在缓冲已满的情况，才会阻塞向缓冲信道（Buffered Channel）发送数据。同样，只有在缓冲为空的时候，才会阻塞从缓冲信道接收数据。
+	通过向 make 函数再传递一个表示容量的参数（指定缓冲的大小），可以创建缓冲信道。
+	ch := make(chan type, capacity) 无缓冲信道的容量默认为 0
+
+	***/
+	ch1 := make(chan int, 2) // 不会发生死锁，因为信道空间未满，未阻塞
+	ch1 <- 5
+	fmt.Println(<-ch1)
+
+	//单向信道
+	//前讨论的信道都是双向信道，即通过信道既能发送数据，又能接收数据。其实也可以创建单向信道，这种信道只能发送或者接收数据。
+	//var read_test <-chan int 单项通道 只支持 读
+	//var write_test chan<- int 单项通道 只支持 写
+	//var chan_test chan 双项通道
+
+	/***关闭信道 数据发送方可以关闭信道，通知接收方这个信道不再有数据发送过来。
+	当从信道接收数据时，接收方可以多用一个变量来检查信道是否已经关闭。
+	v, ok := <- ch
+	只有发送者才能关闭信道，而接收者不能。
+	信道与文件不同，通常情况下无需关闭它们。只有在必须告诉接收者不再有需要发送的值时才有必要关闭，例如终止一个 range 循环。
+
+	***/
+
+	ch2 := make(chan int)
+	go producer(ch2) //往信道里面一直循环写入数据。 和主协程for循环并行。主会一直读数据，知道写入端终止。
+	for {
+		v, ok := <-ch2
+		if ok == false {
+			break
+		}
+		fmt.Println("Received ", v, ok)
+	}
+
+	//range 循环从信道 ch 接收数据，直到该信道关闭。一旦关闭了 ch，循环会自动结束。
+	ch3 := make(chan int)
+	go producer(ch3)
+
+	for v := range ch3 {
+		fmt.Println("Received", v)
+	}
+
+	//长度 vs 容量
+	//缓冲信道的容量是指信道可以存储的值的数量。我们在使用 make 函数创建缓冲信道的时候会指定容量大小。
+	//缓冲信道的长度是指信道中当前排队的元素个数。
+
+	ch4 := make(chan string, 3)
+	ch4 <- "naveen"
+	ch4 <- "paul"
+	fmt.Println("capacity is", cap(ch4))
+	fmt.Println("length is", len(ch4))
+	fmt.Println("read value", <-ch4) //读取一个就处理一下，缓冲隧道里面排队的长度就减少一个。
+	fmt.Println("new length is", len(ch4))
+}
+
+/***
+结果集:
+root@e7939faf8694:/go/src/LessonGo/class_tour/test4concurrency# go run channels.go
+channel a is nil, going to define it
+Type of a is chan int
+-5 17 12
+5
+Received  0 true
+Received  1 true
+Received  2 true
+Received  3 true
+Received  4 true
+Received  5 true
+Received  6 true
+Received  7 true
+Received  8 true
+Received  9 true
+Received 0
+Received 1
+Received 2
+Received 3
+Received 4
+Received 5
+Received 6
+Received 7
+Received 8
+Received 9
+capacity is 3
+length is 2
+read value naveen
+new length is 1
+root@e7939faf8694:/go/src/LessonGo/class_tour/test4concurrency#
+
+***/
---
+++ class_tour/test4concurrency/goroutines.go
@@ -0,0 +1,59 @@
+package main
+
+/***
+并发入门：
+Go 是并发式语言，而不是并行式语言。
+并发是指立即处理多个任务的能力。例如处理一个任务停下来处理别的任务，处理完毕再回来处理之前的任务。
+并行是指同时处理多个任务。例如处理一个任务，同时处理另外一个任务。一般指的是多核。
+多核的并行系统上，并行运行的组件之间可能需要相互通信。所以，并行不一定会加快运行速度。
+
+Go 编程语言原生支持并发。Go 使用 Go 协程（Goroutine） 和信道（Channel）来处理并发。
+goroutines go协程
+Go 协程是与其他函数或方法一起并发运行的函数或方法。Go 协程可以看作是轻量级线程。
+Go 协程相比于线程的优势：
+1，相比线程而言，Go 协程的成本极低。堆栈大小只有若干 kb，并且可以根据应用的需求进行增减。而线程必须指定堆栈的大小，其堆栈是固定不变的。
+2，Go 协程会复用（Multiplex）数量更少的 OS 线程。即使程序有数以千计的 Go 协程，也可能只有一个线程。
+如果该线程中的某一 Go 协程发生了阻塞（比如说等待用户输入），那么系统会再创建一个 OS 线程，并把其余 Go 协程都移动到这个新的 OS 线程。
+所有这一切都在运行时进行。
+3，Go 协程使用信道（Channel）来进行通信。信道用于防止多个协程访问共享内存时发生竞态条件（Race Condition）。
+信道可以看作是 Go 协程之间通信的管道。
+***/
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello() {
+
+	fmt.Println("This is hello world go routine")
+}
+
+//启动多个 Go 协程
+func hello2() {
+
+	fmt.Println("This is hello world another go routine")
+}
+
+func main() {
+	// 主函数和协程会并发执行，但是主函数不会等待协程是否完成。
+	//启动一个新的协程时，协程的调用会立即返回。与函数不同，程序控制不会去等待 Go 协程执行完毕。
+	//在调用 Go 协程之后，程序控制会立即返回到代码的下一行，忽略该协程的任何返回值。
+	//如果希望运行其他 Go 协程，Go 主协程必须继续运行着。如果 Go 主协程终止，则程序终止，于是其他 Go 协程也不会继续运行。
+	go hello()
+	go hello2()
+	//在 Go 主协程中使用休眠，以便等待其他协程执行完毕，这种方法只是用于理解 Go 协程如何工作的技巧。信道可用于在其他协程结束执行之前，阻塞 Go 主协程。
+	time.Sleep(2 * time.Second)
+	fmt.Println("This is the hello main function")
+
+}
+
+/***
+结果集：
+root@e7939faf8694:/go/src/LessonGo/class_tour/test4concurrency# go run goroutines.go
+This is hello world go routine
+This is hello world another go routine
+This is the hello main function
+root@e7939faf8694:/go/src/LessonGo/class_tour/test4concurrency#
+
+***/
